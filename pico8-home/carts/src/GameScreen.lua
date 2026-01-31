a02c76fe17a3557f1bba1ce2878588c9f8e745b3
Patrick Calahan <github@pcal.net>
2026-01-30 19:44:21 -0800

    checkpoint

--- pico8-home/carts/src/GameScreen.lua
+++ pico8-home/carts/src/GameScreen.lua
@@ -2,11 +2,18 @@ local GameScreen = {}
 GameScreen.new = function()
     local self = {}
 
+    local POSTER_TOP_DISPLAY_POS = 9
+    local POSTER_BOT_DISPLAY_POS = 118
+    local POSTER_NEW_DISPLAY_POS = -30
+    local POSTER_MOVE_SPEED = 4
+
     self.scrollPos = 0
     self.targetPos = 1
     self.canPress = true
     self.secondsRemaining = 60.0
     self._last_time = time()
+    self.posterY = POSTER_NEW_DISPLAY_POS
+    self.targetPosterY = POSTER_TOP_DISPLAY_POS
 
     self.catList = {}
     self.catListSize = 10
@@ -33,19 +40,28 @@ GameScreen.new = function()
     function self.draw()
         cls(15) -- offwhite background
 
+        -- draw black header rectangle
+        local header_h = 9
+        local header_w = 84
+        local header_x = (128 - header_w) / 2
+        local header_y = 0
+
+
         -- draw poster at the top
         local rw, rh = 84, 44
         local rx = (128 - rw) / 2
-        local ry = 0
-        rectfill(rx, ry, rx + rw - 1, ry + rh - 1, 7)
-        rectfill(rx, ry, rx, ry + rh - 1, 5)                   -- left
-        rectfill(rx + rw - 1, ry, rx + rw - 1, ry + rh - 1, 5) -- right
-        rectfill(rx, ry + rh - 1, rx + rw - 1, ry + rh - 1, 5) -- bottom
+        rectfill(rx, self.posterY, rx + rw - 1, self.posterY + rh - 1, 7)
+        rect(rx, self.posterY, rx + rw - 1,  self.posterY + rh - 1, 5)
+        self.print_center_top(self.posters[1].name, 1, 2, 1, self.posterY + 9)
+        self.print_center_top("blue fur", 2, 4, 1, self.posterY + 9)
+        self.print_center_top("green eyes", 3, 4, 1, self.posterY + 9)
 
-        self.print_center_top("lost cat:", 0, 2)
-        self.print_center_top(self.posters[1].name, 1, 2)
-        self.print_center_top("blue fur", 2, 4)
-        self.print_center_top("green eyes", 3, 4)
+
+        rectfill(header_x - 1, header_y, header_x + 1 + header_w - 1, header_y + header_h, 0)
+        
+        self.print_center_top("lost cat!", 0, 1, 10) -- yellow text
+
+
 
         -- draw icons: left (sprite 8) with catsRemaining, and clock (sprite 10) right with seconds
         palt(0, false) -- black is black, beige is transparent
@@ -128,15 +144,26 @@ GameScreen.new = function()
             end
         end
 
+        -- adjust posterY to 'catch up' with targetPosterY
+        local posterDiff = self.targetPosterY - self.posterY
+        local posterDist = abs(posterDiff)
+        if posterDist > 0 then
+            if posterDist <= POSTER_MOVE_SPEED then
+                self.posterY = self.targetPosterY
+            else
+                self.posterY = self.posterY + sgn(posterDiff) * POSTER_MOVE_SPEED
+            end
+        end
+
     end
 
- function self.print_center_top(text, line, y_margin)
+ function self.print_center_top(text, line, y_margin, color, base_y)
     local TEXT_HEIGHT = 6
     local w = #text * 8
     local x = (128 - w) / 2
-    local y = (line * TEXT_HEIGHT) + (y_margin or 0)
+    local y = (base_y or 0) + (line * TEXT_HEIGHT) + (y_margin or 0)
     local wide_text = "\^w"..text    
-    print(wide_text, x, y, 1, true)
+    print(wide_text, x, y, color or 1, true)
 end
 
 
